1e0ef7620cc7ad2737c82734ea6df38530f2d6bb
Alan Chatfield <47448740+AlanChatfield@users.noreply.github.com>
2021-02-19 15:44:45 +0000

    Update shared.js

--- static/js/shared.js
+++ static/js/shared.js
@@ -22,6 +22,8 @@ $(document).ready(function(){
 	});
 	
 	if(document.location.search.length > 4) {
+		var query = window.location.search.substring(1); 
+		var vars = query.split("&"); 
 		for (var i=0;i < vars.length;i++) { 
 			var pair = vars[i].split("="); 
 			if( (pair[0].substring(0, 4) == "utm_") && (pair[1] != "")) {
